09706b5e2ae2840b0263be977ca365f5d14e9eee
Ben <benxv@yahoo.com>
2014-12-12 17:12:13 -0800

    No default value for data field of EventProcessingError

--- djstripe/migrations/0003_auto_20141212_0254.py
+++ djstripe/migrations/0003_auto_20141212_0254.py
@@ -16,6 +16,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='eventprocessingexception',
             name='data',
-            field=jsonfield.fields.JSONField(default=__builtin__.dict),
+            field=jsonfield.fields.JSONField(),
         ),
     ]
